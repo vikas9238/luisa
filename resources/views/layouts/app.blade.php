ab6c936b51d0c1ef281f5043dfaa03383e64240a
vikas9238 <vikashkumar9238@gmail.com>
2024-03-15 19:17:25 +0530

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title> Luisa Pharmaceutical PVT LTD </title>
-        <link href="assets/img/favicon.png" rel="icon">
-        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">        
         <!--<link href="{{ asset('aos/aos.css') }}" rel="stylesheet">-->
         <link href="{{ asset('animate.css/animate.min.css') }}" rel="stylesheet">
@@ -19,13 +17,20 @@
         <link href="{{ asset('remixicon/remixicon.css') }}" rel="stylesheet">
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
         <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('favicon//site.webmanifest') }}">
+        <link rel="mask-icon" href="{{ asset('favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
     </head>
     <body>
         <!-- ======= Top Bar ======= -->
         <div id="topbar" class="fixed-top d-flex align-items-center topbar-inner-pages">
             <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
                 <div class="contact-info d-flex align-items-center">
-                    <i class="bi bi-envelope-fill"></i><a href="mailto:contact@example.com">info@luisapharma.com</a>
+                    <i class="bi bi-envelope-fill"></i><a href="mailto:info@luisapharma.com">info@luisapharma.com</a>
                     <i class="bi bi-phone-fill phone-icon"></i>7982828503
                 </div>
             </div>
